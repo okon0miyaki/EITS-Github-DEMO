6aa5cb5351927fa62557019f72534379c049ec29
RnG <113661821+okon0miyaki@users.noreply.github.com>
2024-11-13 09:49:47 +0700

    Update Sort.java

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -52,10 +52,7 @@ public class Sort {
 	}
 	
 	static void printArray(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
+		//TODO : Print the Array
 	}
         
 	public static void main(String[] args) {
